1c96ef7a3eea18055d28e0251562d67eeb89cbb8
ortigueras <ortiguerasmark@gmail.com>
2025-01-06 10:11:38 +0800

    osk 2M

--- DSA/GrandPrize2M.cs
+++ DSA/GrandPrize2M.cs
@@ -12,9 +12,26 @@ namespace DSA
 {
     public partial class GrandPrize2M : Form
     {
+        public static GrandPrize2M grandPrizeInstance;
         public GrandPrize2M()
         {
             InitializeComponent();
+        }
+
+        private void GrandPrize2M_Load(object sender, EventArgs e)
+        {
+            if (Character.playerCharacter == "Jeff")
+            {
+                pictureBox1.Image = Properties.Resources.GIF_Intense_P1;
+            }
+            else if (Character.playerCharacter == "Golfy")
+            {
+                pictureBox1.Image = Properties.Resources.GIF_Intense_P2;
+            }
+            else
+            {
+                pictureBox1.Image = Properties.Resources.GIF_Intense_P3;
+            }
         }
     }
 }
